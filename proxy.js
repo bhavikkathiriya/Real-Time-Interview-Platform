efe584057bbbacbba46d2083a26cfe8bdecffec3
Bhavik_Kathiriya <bhavikkathiriya1428@gmail.com>
2026-07-07 20:12:36 +0530

    feat: implement onboarding flow and interviewer profile UI

--- proxy.js
+++ proxy.js
@@ -1,3 +1,4 @@
+import arcjet, { detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware , createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from "next/server";
 
@@ -8,7 +9,24 @@ const isProtectedRoute = createRouteMatcher([
   "/onboarding(.*)",
 ]);
 
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:PREVIEW"],
+    }),
+  ],
+});
+
 export default clerkMiddleware(async (auth, req) => {
+
+  const decision = await aj.protect(req);
+  if (decision.isDenied()) {
+    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  }
+
   const { userId } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
